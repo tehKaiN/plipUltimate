e449f8303052b92c2cfe56f34050608bc030a928
KaiN <kain@piwnica.ws>
2017-01-19 20:41:00 +0100

    Fixed hex2puf on m68k
    Crashes were stack-related. Changed big allocs to heap.

--- hex2puf/main.c
+++ hex2puf/main.c
@@ -7,9 +7,9 @@
 #define BOOT_NRWW_PAGE_COUNT 32
 #define BOOT_RWW_PAGE_COUNT (BOOT_PAGE_COUNT - BOOT_NRWW_PAGE_COUNT)
 
+unsigned char s_pFirmware[BOOT_PAGE_BYTE_SIZE * BOOT_RWW_PAGE_COUNT];
 int main(int lArgCount, char *pArgs[]) {
 	FILE *pHexFile, *pPuf;
-	unsigned char pFirmware[BOOT_PAGE_BYTE_SIZE * BOOT_RWW_PAGE_COUNT];
 	char szDataHexes[34+1];
 	unsigned short int uwLine, uwSize, uwByteAddr, uwType, i;
 	char bWhiteSpace;
@@ -46,7 +46,7 @@ int main(int lArgCount, char *pArgs[]) {
 		
 		// Put read data into firmware buffer
 		for(i = 0; i != uwSize; ++i) {
-			sscanf(&szDataHexes[2*i], "%2hhx", (char*)&pFirmware[uwByteAddr + i]);
+			sscanf(&szDataHexes[2*i], "%2hhx", (char*)&s_pFirmware[uwByteAddr + i]);
 		}
 
 		// Skip newline chars
@@ -58,7 +58,7 @@ int main(int lArgCount, char *pArgs[]) {
 	fclose(pHexFile);
 	
 	// Convert endianness
-	pFirmwareWords = (unsigned short int *)pFirmware;
+	pFirmwareWords = (unsigned short int *)s_pFirmware;
 	for(i = 0; i != BOOT_PAGE_WORD_SIZE*BOOT_RWW_PAGE_COUNT; ++i)
 		pFirmwareWords[i] = (pFirmwareWords[i] >> 8) | ((pFirmwareWords[i] & 0xFF) << 8);
 	
@@ -70,11 +70,11 @@ int main(int lArgCount, char *pArgs[]) {
 	ubRealPages = 0;
 	for(ubPage = 0; ubPage != BOOT_RWW_PAGE_COUNT; ++ubPage) {
 		for(i = 0; i != BOOT_PAGE_BYTE_SIZE; ++i) {
-			if(pFirmware[ubPage*BOOT_PAGE_BYTE_SIZE + i]) {
+			if(s_pFirmware[ubPage*BOOT_PAGE_BYTE_SIZE + i]) {
 				// Write only non-empty pages
 				++ubRealPages;
 				fwrite(&ubPage, 1, 1, pPuf);
-				fwrite(&pFirmware[ubPage*BOOT_PAGE_BYTE_SIZE], BOOT_PAGE_BYTE_SIZE, 1, pPuf);
+				fwrite(&s_pFirmware[ubPage*BOOT_PAGE_BYTE_SIZE], BOOT_PAGE_BYTE_SIZE, 1, pPuf);
 				break;
 			}
 		}
@@ -85,6 +85,6 @@ int main(int lArgCount, char *pArgs[]) {
 	fseek(pPuf, 0, SEEK_SET);
 	fwrite(&ubRealPages, 1, 1, pPuf);
 	
-	// fwrite(pFirmware, BOOT_PAGE_BYTE_SIZE * BOOT_RWW_PAGE_COUNT, 1, pPuf);
+	// fwrite(s_pFirmware, BOOT_PAGE_BYTE_SIZE * BOOT_RWW_PAGE_COUNT, 1, pPuf);
 	fclose(pPuf);
 }
