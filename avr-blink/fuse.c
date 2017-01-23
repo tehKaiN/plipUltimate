d06b907e938443e07fffb8183f7165c85295b4b6
KaiN <kain@piwnica.ws>
2017-01-23 19:17:41 +0100

    Removed avr-blink program, updated GPLv2 notice in all files

--- avr-blink/fuse.c
+++
@@ -1,7 +0,0 @@
-#include <avr/io.h>
-
-FUSES = {
-    .low = 0xE0,
-    .high = 0xD8,
-    .extended = EFUSE_DEFAULT
-};
--- avr/src/pb_proto.h
+++ avr/src/pb_proto.h
@@ -1,27 +1,20 @@
 /*
- * pb_proto.h - avr implementation of the plipbox protocol
+ * This file is part of PlipUltimate.
+ * Based on PlipBox by Christian Vogelgsang <chris@vogelgsang.org>
  *
- * Written by
- *  Christian Vogelgsang <chris@vogelgsang.org>
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- * This file is part of plipbox.
- * See README for copyright notice.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
- *  02111-1307  USA.
- *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
 #ifndef PBPROTO_H
